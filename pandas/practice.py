06202f249e94a01824523e2df05c7f6f5974d676
Your Name <you@example.com>
2022-07-01 14:55:04 +0530

    added a practice.py file

---
+++ pandas/practice.py
@@ -0,0 +1,35 @@
+from textwrap import indent
+import pandas as pd
+import json
+
+
+# df = pd.read_csv('/home/navgurukul/Desktop/pythonClass/pandas/data.csv')
+# pf = pd.DataFrame(df)
+
+# print(pf.loc[[0,1,2,3]])
+
+dic= {"name": [1,1,3,4], "work": [5,5,7,8], "city": ["k","k","g","o"]}
+
+with open("practice.json", "w") as f:
+    json.dump(dic, f, indent=2)
+
+df = pd.read_json('/home/navgurukul/Desktop/pythonClass/practice.json')
+
+# # pf = pd.DataFrame(dic, index= ["day1","day2","day3","day4"] )
+# print(df.duplicated())
+# new_df= df.drop_duplicates()
+
+# print(new_df)
+
+
+
+df.loc[0,"name"]= "radhi"
+
+for i in df.index:
+    if df.loc[i,"city"] == "k":
+        df.loc[i,"city"] = "R"
+
+print(df)
+
+
+
